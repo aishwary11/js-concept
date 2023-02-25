b33191bfa4a06cbe0278bff07629e17a88b06d8a
aishwary11 <aishwary46@gmail.com>
2023-02-25 12:41:27 +0530

    constructor added

--- constructor.js
+++ constructor.js
@@ -19,3 +19,33 @@ class Model extends Car {
 }
 
 console.log(new Model("lambo", "calarado").show());
+
+
+class Animal {
+  constructor (name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  }
+}
+
+class Dog extends Animal {
+  constructor (name, breed) {
+    super(name);
+    this.breed = breed;
+  }
+
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+
+  breedInfo() {
+    console.log(`${this.name} is a ${this.breed}.`);
+  }
+}
+
+const dog = new Dog('Fido', 'Labrador');
+dog.speak(); // logs 'Fido barks.'
+dog.breedInfo(); // logs 'Fido is a Labrador.'
