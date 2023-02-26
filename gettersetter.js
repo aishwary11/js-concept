323c5ba0628d31899ec005bc79d4aba25c574505
aishwary11 <aishwary46@gmail.com>
2023-02-26 20:36:51 +0530

    generators & getter setter

---
+++ gettersetter.js
@@ -0,0 +1,15 @@
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  }
+};
+// set name in fullname
+user.fullName = "Alice Cooper";
+console.log(user.fullName);
