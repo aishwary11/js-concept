bb545e4ba756db190e878336e19a310c57909d36
aishwary11 <aishwary46@gmail.com>
2023-03-03 11:22:07 +0530

    caching added

--- cachingurl.js
+++ cachingurl.js
@@ -8,11 +8,11 @@ async function fetchData(url) {
   cache[url] = data;
   return cache[url];
 }
-console.time();
 async function fetcher() {
   await fetchData('https://jsonplaceholder.typicode.com/posts');
   await fetchData('https://jsonplaceholder.typicode.com/posts');
   await fetchData('https://jsonplaceholder.typicode.com/posts');
 };
+console.time();
 fetcher();
 console.timeEnd();
