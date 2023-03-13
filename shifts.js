c2fd55528ad3a480427cdf05368e3d0ee8071853
aishwary11 <aishwary46@gmail.com>
2023-03-13 15:21:32 +0530

    shifts added

---
+++ shifts.js
@@ -0,0 +1,8 @@
+/**
+ * here we shift 10 binary to right
+ */
+const ten = 10; // 1010
+const one = 1;  // 0001
+const two = 2;  // 0010
+console.log(ten >> two); // 0010
+console.log(ten << two); // 101000
