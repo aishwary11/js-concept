273c47d11eb4b3bc387453e60980baab8e1824fb
aishwary11 <aishwary46@gmail.com>
2023-04-07 15:23:34 +0530

    test

---
+++ removeduplicatechar.js
@@ -0,0 +1,13 @@
+const input = 'teeeessssssts';
+const output = 'tts';
+let result = '';
+let temp = '';
+for (let i = 0; i < input.length; i++) {
+    if (input[i] !== temp && input[i + 1] !== input[i]) {
+        result += input[i];
+        temp = input[i];
+    } else {
+        temp = input[i];
+    }
+}
+console.log(result);
