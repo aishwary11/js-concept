2e54c4c521728f7466c76d6e0c1433d546550573
aishwary11 <aishwary46@gmail.com>
2023-05-05 21:36:36 +0530

    call by ref vs call by val

---
+++ callbyrefvscallbyval.js
@@ -0,0 +1,26 @@
+// call by value
+let obj1 = {
+  name: "Aish"
+};
+let obj2 = obj1;
+obj2.name = "Shah";
+console.log(obj1, obj2);
+console.log(obj1 === obj2);
+// call by ref
+let obj3 = {
+  name: "Aish"
+};
+console.log(obj1 === obj3);
+
+
+
+
+
+
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [1, 2, 3, 4];
+console.log(arr1 == arr2);
+
+let arr3 = arr1;
+console.log(arr3 == arr1);
--- test.js
+++ test.js
@@ -20,4 +20,28 @@
 const x = {
   foo: "bar"
 };
-console.log(Object.hasOwn(x,"foo"))
+console.log(Object.hasOwn(x, "foo"));
+
+function reverseString(str) {
+  // remove all non-alphabetic characters using regular expression
+  const newStr = str.replace(/[^A-Za-z]/g, '');
+  // convert the new string to array, reverse it and join again to get the reversed string
+  const reversedStr = newStr.split('').reverse().join('');
+  // loop through the original string and add the non-alphabetic characters at their original positions
+  let j = 0;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (!/[A-Za-z]/.test(str[i])) {
+      // if the character is non-alphabetic, add it to the result
+      result += str[i];
+    } else {
+      // if the character is alphabetic, add the reversed alphabetic character to the result
+      result += reversedStr[j];
+      j++;
+    }
+  }
+  return result;
+}
+
+console.log(reverseString('Aish*wa#ry')); // Output: yraw*hs#iA
+
