772e2feebea9be004a8056a0fca9d7d424fed28e
aishwary11 <aishwary46@gmail.com>
2023-05-08 14:41:48 +0530

    debounce & throttling

---
+++ debouncingvsthrottling.js
@@ -0,0 +1,37 @@
+const debounce = (func, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+const doSearch = (query) => console.log(`Searching for "${query}"...`);
+
+const debouncedSearch = debounce(doSearch, 2000);
+
+debouncedSearch('apple');
+debouncedSearch('orange');
+debouncedSearch('pear');
+
+/**
+ * Throttling
+ */
+
+const throttleFunc = (func, delay = 5000) => {
+  let shouldWait = false;
+  return (...args) => {
+    if (shouldWait) return;
+    func(...args);
+    shouldWait = true;
+    setTimeout(() => {
+      shouldWait = false;
+    }, delay);
+  };
+};
+
+const cons = () => console.log('logger throttle');
+
+throttleFunc(cons(), 3000);
