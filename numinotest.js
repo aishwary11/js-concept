4ef5c1ab0d15d9690c811aa4c7a33e1304c53e1a
aishwary11 <aishwary46@gmail.com>
2023-06-05 12:29:27 +0530

    code optimize for test

--- numinotest.js
+++ numinotest.js
@@ -24,20 +24,43 @@ console.log(maxOccurChar('repeated'));
 // Sum = 10
 // {1,2,3,8,9,0,7,12,15}
 
+/**
+ * Method 1
+ */
+
+// function sum(arr) {
+//   let arrSum = 10;
+//   let pairs = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === arrSum) {
+//         pairs.push([arr[i], arr[j]]);
+//       }
+//     }
+//   }
+//   return pairs;
+// }
+
+/**
+ * Method 2
+ */
 function sum(arr) {
   let arrSum = 10;
   let pairs = [];
+  const set = new Set();
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === arrSum) {
-        pairs.push([arr[i], arr[j]]);
-      }
+    let complement = arrSum - arr[i];
+    if (set.has(complement)) {
+      pairs.push([arr[i], complement]);
     }
+    set.add(arr[i]);
   }
   return pairs;
 }
 
-console.log(sum([1, 2, 3, 8, 9, 0, 7, 12, 15]))
+console.time("Sum");
+console.log(sum([1, 2, 3, 8, 9, 0, 7, 12, 15]));
+console.timeEnd("Sum")
 
 
 
