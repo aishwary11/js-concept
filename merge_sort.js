653890e3b75ce79a9186248793610bc5297a5934
Aishwary Shah <49820149+aishwary11@users.noreply.github.com>
2024-07-31 22:48:44 +0530

    merge sort change

--- merge_sort.js
+++ merge_sort.js
@@ -1,18 +1,26 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
-  const mid = Math.floor(arr.length / 2);
-  const left = mergeSort(arr.slice(0, mid));
-  const right = mergeSort(arr.slice(mid));
-  return merge(left, right);
-};
 function merge(left, right) {
-  let sorted = [];
-  while (left.length && right.length) {
-    if (left[0] < right[0]) sorted.push(left.shift());
-    else sorted.push(right.shift());
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex++]);
+    } else {
+      result.push(right[rightIndex++]);
+    }
   }
-  return [...sorted, ...left, ...right];
-};
+  while (leftIndex < left.length) result.push(left[leftIndex++]);
+  while (rightIndex < right.length) result.push(right[rightIndex++]);
+  return result;
+}
+
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+}
 console.time();
-console.log(mergeSort([7, 8, 4, 1, 2, 5]));
+console.log(mergeSort([4, 2, 6, 1, 3, 5]));
 console.timeEnd();
