e41f41a9374f68bb0bd0809d602be070f95bc119
aishwary11 <aishwary46@gmail.com>
2024-09-08 02:10:57 +0530

    optimized

--- removeduplicatechar.js
+++ removeduplicatechar.js
@@ -1,11 +1,8 @@
 const input = 'teeeessssssts';
 const output = 'tts';
-let result = '';
-let temp = '';
-for (let i = 0; i < input.length; i++) {
-    if (input[i] !== temp && input[i + 1] !== input[i]) {
-        result += input[i];
-        temp = input[i];
-    } else temp = input[i];
+let result = '', prev = '';
+for (const char of input) {
+    if (char !== prev) result += char;
+    prev = char;
 }
 console.log(result);
